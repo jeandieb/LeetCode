b1a27b6d5745819ffa9e99b4806c5af6fb435bd7
jeandieb <45843487+jeandieb@users.noreply.github.com>
2021-03-30 20:37:12 -0700

    Add files via upload

---
+++ IsSubsequence.java
@@ -0,0 +1,29 @@
+public class IsSubsequence
+{
+   public static void main(String[]args)
+   {
+      System.out.println("hello");
+      System.out.println(isSubsequence("b", "abc"));
+   }
+   
+   public static boolean isSubsequence(String s, String t)
+   {
+      if(t.length() < s.length()) return false; 
+      if(t.equals(s)) return true;
+      String ans = "";
+      int j = 0;
+      int i = 0;
+      while(i < s.length() && j < t.length())
+      {
+         if (s.charAt(i) == t.charAt(j)) 
+         {
+            ans += s.charAt(i);
+            i++;
+         }
+         j++;
+
+      }
+
+      return i == s.length();
+   }
+}
